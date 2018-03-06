1fff32f1de6549860dfe47b7a3bcba94a578035d
awatef <awatef@localhost.localdomain>
2018-03-06 17:29:27 +0100

    test webhook

--- src/main/java/com/hrdatabank/tipisign/controller/BotController.java
+++ src/main/java/com/hrdatabank/tipisign/controller/BotController.java
@@ -100,74 +100,75 @@ public class BotController {
 
 		LinkedHashMap<String, String> hm = new LinkedHashMap<>();
 		System.out.println(intentName);
-		// switch (intentName.toLowerCase()) {
-		// case "carousel":
-		//
-		// String imageUrl = createUri("/static/buttons/1040.jpg");
-		// CarouselTemplate carouselTemplate = new CarouselTemplate(Arrays.asList(
-		// new CarouselColumn(imageUrl, "hoge", "fuga",
-		// Arrays.asList(new URIAction("Go to line.me", "https://line.me"),
-		// new URIAction("Go to line.me", "https://line.me"),
-		// new PostbackAction("Say hello1", "hello こんにちは"))),
-		// new CarouselColumn(imageUrl, "hoge", "fuga",
-		// Arrays.asList(new PostbackAction("言 hello2", "hello こんにちは", "hello こんにちは"),
-		// new PostbackAction("言 hello2", "hello こんにちは", "hello こんにちは"),
-		// new MessageAction("Say message", "Rice=米"))),
-		// new CarouselColumn(imageUrl, "Datetime Picker", "Please select a date, time
-		// or datetime",
-		// Arrays.asList(
-		// new DatetimePickerAction("Datetime", "action=sel", "datetime",
-		// "2017-06-18T06:15",
-		// "2100-12-31T23:59", "1900-01-01T00:00"),
-		// new DatetimePickerAction("Date", "action=sel&only=date", "date",
-		// "2017-06-18",
-		// "2100-12-31", "1900-01-01"),
-		// new DatetimePickerAction("Time", "action=sel&only=time", "time", "06:15",
-		// "23:59",
-		// "00:00")))));
-		// TemplateMessage templateMessage = new TemplateMessage("Carousel alt text",
-		// carouselTemplate);
-		// PushMessage pushMessage = new PushMessage(userId, templateMessage);
-		// LineMessagingServiceBuilder.create(channelToken).build().pushMessage(pushMessage).execute();
-		//
-		// break;
-		//
-		// case "menu":
-		//
-		// hm.put("Osaka", "osaka");
-		// hm.put("Tokyo", "tokyo");
-		// hm.put("London", "london");
-		//
-		// typeBRecursiveChoices(
-		// "https://lh3.googleusercontent.com/oKsgcsHtHu_nIkpNd-mNCAyzUD8xo68laRPOfvFuO0hqv6nDXVNNjEMmoiv9tIDgTj8=w170",
-		// " boldTitle", " normalTitle", hm, channelToken, userId);
-		// logger.info("paris :" + customerMessage);
-		//
-		// break;
-		// case "type b":
-		//
-		// typeBRecursiveChoices(
-		// "https://lh3.googleusercontent.com/oKsgcsHtHu_nIkpNd-mNCAyzUD8xo68laRPOfvFuO0hqv6nDXVNNjEMmoiv9tIDgTj8=w170",
-		// " boldTitle", " normalTitle", hm, channelToken, userId);
-		// logger.info("see more :", customerMessage);
-		// break;
-		// case "b2":
-		// hm = new LinkedHashMap<>();
-		// hm.put("Osaka", "osaka");
-		// hm.put("Tokyo", "tokyo");
-		// hm.put("London", "london");
-		// typeBChoices(
-		// "https://lh3.googleusercontent.com/oKsgcsHtHu_nIkpNd-mNCAyzUD8xo68laRPOfvFuO0hqv6nDXVNNjEMmoiv9tIDgTj8=w170",
-		// " boldTitle", " normalTitle", hm, " nextOrSeeMore", " nextOrSeeMoreAnswer",
-		// channelToken, userId);
-		// logger.info("London :", customerMessage);
-		// break;
-		//
-		// default:
-		// sendAlertViaSlack(userId, timestamp, customerMessage);
-		// logger.info("slack :" + customerMessage);
-		// break;
-		// }
+		switch (intentName.toLowerCase()) {
+		case "carousel":
+
+			String imageUrl = createUri("/static/buttons/1040.jpg");
+			CarouselTemplate carouselTemplate = new CarouselTemplate(Arrays.asList(
+					new CarouselColumn(imageUrl, "hoge", "fuga",
+							Arrays.asList(new URIAction("Go to line.me", "https://line.me"),
+									new URIAction("Go to line.me", "https://line.me"),
+									new PostbackAction("Say hello1", "hello こんにちは"))),
+					new CarouselColumn(imageUrl, "hoge", "fuga",
+							Arrays.asList(new PostbackAction("言 hello2", "hello こんにちは", "hello こんにちは"),
+									new PostbackAction("言 hello2", "hello こんにちは", "hello こんにちは"),
+									new MessageAction("Say message", "Rice=米"))),
+					new CarouselColumn(imageUrl, "Datetime Picker", "Please select a date, time	 or datetime",
+							Arrays.asList(
+									new DatetimePickerAction("Datetime", "action=sel", "datetime", "2017-06-18T06:15",
+											"2100-12-31T23:59", "1900-01-01T00:00"),
+									new DatetimePickerAction("Date", "action=sel&only=date", "date", "2017-06-18",
+											"2100-12-31", "1900-01-01"),
+									new DatetimePickerAction("Time", "action=sel&only=time", "time", "06:15", "23:59",
+											"00:00")))));
+			TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
+			PushMessage pushMessage = new PushMessage(userId, templateMessage);
+			LineMessagingServiceBuilder.create(channelToken).build().pushMessage(pushMessage).execute();
+
+			break;
+
+		case "menu":
+
+			hm.put("Osaka", "osaka");
+			hm.put("Tokyo", "tokyo");
+			hm.put("London", "london");
+
+			typeBRecursiveChoices(
+					"https://lh3.googleusercontent.com/oKsgcsHtHu_nIkpNd-mNCAyzUD8xo68laRPOfvFuO0hqv6nDXVNNjEMmoiv9tIDgTj8=w170",
+					" boldTitle", " normalTitle", hm, channelToken, userId);
+			logger.info("paris :" + customerMessage);
+
+			break;
+
+		case "type b":
+
+			typeBRecursiveChoices(
+					"https://lh3.googleusercontent.com/oKsgcsHtHu_nIkpNd-mNCAyzUD8xo68laRPOfvFuO0hqv6nDXVNNjEMmoiv9tIDgTj8=w170",
+					" boldTitle", " normalTitle", hm, channelToken, userId);
+			logger.info("see more :", customerMessage);
+
+			break;
+
+		case "b2":
+
+			hm = new LinkedHashMap<>();
+			hm.put("Osaka", "osaka");
+			hm.put("Tokyo", "tokyo");
+			hm.put("London", "london");
+			typeBChoices(
+					"https://lh3.googleusercontent.com/oKsgcsHtHu_nIkpNd-mNCAyzUD8xo68laRPOfvFuO0hqv6nDXVNNjEMmoiv9tIDgTj8=w170",
+					" boldTitle", " normalTitle", hm, " nextOrSeeMore", " nextOrSeeMoreAnswer", channelToken, userId);
+			logger.info("London :", customerMessage);
+
+			break;
+
+		default:
+
+			// sendAlertViaSlack(userId, timestamp, customerMessage);
+			logger.info("slack :" + customerMessage);
+
+			break;
+		}
 
 		return json;
 
