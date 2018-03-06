27d675914481c211f7a7a0b911f2258988f38a34
awatef <awatef@localhost.localdomain>
2018-03-06 12:24:32 +0100

    0.0.9

--- src/main/java/com/hrdatabank/tipisign/controller/BotController.java
+++ src/main/java/com/hrdatabank/tipisign/controller/BotController.java
@@ -68,10 +68,10 @@ public class BotController {
 	@ResponseBody
 	@RequestMapping(value = "/webhook")
 	private void webhook(@RequestBody String obj)  {
+		System.out.println("helooooooooooo");
 	//	String channelToken = "YeGR+Tx/wXpHTEto2B3faYXy16D+9ACtLW7yiUqqDAtFeX0nW5AEgHNcUYfxDL4+5QQ1ytWFUv1Ol5+1Pb2wOWk5+44idmCjlP6vancpqEk6q0tNuG4GGOPg5/S/mE4HZiqkBcEJ4F5plHAxfQ0CTwdB04t89/1O/w1cDnyilFU=";
 		 JSONObject bodyJson = new JSONObject(obj);
 		 logger.info("\nJSON BODY FROM DIALOGFLOW \n {}  ", bodyJson, "\n \n");
-		return; 
 	}
 	
 	/**Map<String, Object> webhook(@RequestBody Map<String, Object> obj)
